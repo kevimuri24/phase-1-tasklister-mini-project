478523ce9836457da14b0684e72e9aaf1d022f84
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-07-17 22:58:25 +0300

    Added Delete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+function handleNewTask(task){
+  const li = document.createElement('li')
+  const removeTask = document.createElement('button')
+  removeTask.textContent = 'x'
+  li.textContent = task + ' '
+  li.appendChild(removeTask)
+
+  document.querySelector('#tasks').appendChild(li)
+
+  removeTask.addEventListener('click', (e)=>{
+    e.target.parentNode.remove()
+  })
+}
+
+const submit = document.querySelector('input[type=submit]')
+submit.addEventListener('click', function(e){
+  e.preventDefault()
+  const inputValue = document.querySelector('#new-task-description')
+  handleNewTask(inputValue.value)
+})
